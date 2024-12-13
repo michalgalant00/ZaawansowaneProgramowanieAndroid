c3935e20b4ecd39f954bcb6b3279a82ae2bc22fd
michalgalant00 <s95401@pollub.edu.pl>
2024-12-13 22:47:31 +0100

    04 05.11 - profile screen

---
+++ app/src/main/java/com/example/masterand/models/Profile.kt
@@ -0,0 +1,10 @@
+package com.example.masterand.models
+
+class Profile(
+    var login: String,
+    var email: String,
+    var description: String,
+    var picture: Int, //tmp - potem do zmiany tak, żeby był obrazek
+) {
+
+}
